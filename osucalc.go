7fece3adbd4a32d2fed3a61327200d044728c6cb
SaberStrike <sabersfear@gmail.com>
2022-06-08 18:56:22 +1000

    eee

--- osucalc.go
+++ osucalc.go
@@ -4,27 +4,6 @@ import (
 	"math"
 	"reflect"
 )
-//object for hit300,100,50 windows
-type hitwindowobj struct {
-	Range300 float32
-	Range100 float32
-	Range50  float32
-	OD       float32
-}
-
-type accuracygrade struct {
-	Grade    string
-	Accuracy float32
-	Fullacc  float64
-}
-
-// circle size, approach rate, overall difficulty, health drain
-type basicmapval struct {
-	CS float32
-	AR float32
-	OD float32
-	HP float32
-}
 /*
 convert approach rate to milliseconds
 */
--- other.go
+++ other.go
@@ -1,4 +1,4 @@
-package osumodcalculatorgo_STRUCTS
+package osumodcalculatorgo
 
 //object for hit300,100,50 windows
 type hitwindowobj struct {
